48465c0eb8669a5bfd1af702c1934f9a36f19b3c
pc-dok <info@n4k.at>
2022-10-18 17:17:40 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,4 @@
 terraform {
-
   cloud {
     organization = "N4K"
 
@@ -7,25 +6,12 @@ terraform {
       name = "N4K-Github-AVD-Packer"
     }
   }
-  
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      }
-    azuread = {
-      source  = "hashicorp/azuread"
-      }
-  }
 }
 
 provider "azurerm" {
+  subscription_id = var.ARM_SUBSCRIPTION
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
   features {}
 }
-
-#provider "azurerm" {
-  #subscription_id = var.ARM_SUBSCRIPTION
-  #client_id       = var.ARM_CLIENT_ID
-  #client_secret   = var.ARM_CLIENT_SECRET
-  #tenant_id       = var.ARM_TENANT_ID
- # features {}
-#}
