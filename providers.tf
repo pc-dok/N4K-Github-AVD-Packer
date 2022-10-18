f536a44bc164f4bff2cf318a0c299a76ae29a88a
pc-dok <info@n4k.at>
2022-10-18 17:12:46 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -19,9 +19,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.ARM_SUBSCRIPTION
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
+  #subscription_id = var.ARM_SUBSCRIPTION
+  #client_id       = var.ARM_CLIENT_ID
+  #client_secret   = var.ARM_CLIENT_SECRET
+  #tenant_id       = var.ARM_TENANT_ID
   features {}
 }
