31d5d67bb092fd4fe8da79b0cbb4d9f10d1751d9
pc-dok <info@n4k.at>
2022-10-19 20:48:08 +0200

    Update packers.tf

--- packers.tf
+++ packers.tf
@@ -364,6 +364,148 @@ EOT
   overwrite_on_create = true
 }
 
+resource "github_repository_file" "win11pkrhcl" {
+  repository          = github_repository.packer_windows_avd.name
+  branch              = "main"
+  file                = "windows.pkr.hcl"
+  content             = <<EOT
+# Windows 11  
+variable "client_id" {
+  type        = string
+  description = "Azure Service Principal App ID."
+  sensitive   = true
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Service Principal Secret."
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID."
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID."
+  sensitive   = true
+}
+
+variable "artifacts_resource_group" {
+  type        = string
+  description = "Packer Artifacts Resource Group."
+}
+
+variable "build_resource_group" {
+  type        = string
+  description = "Packer Build Resource Group."
+}
+
+variable "source_image_publisher" {
+  type        = string
+  description = "Windows Image Publisher."
+}
+
+variable "source_image_offer" {
+  type        = string
+  description = "Windows Image Offer."
+}
+
+variable "source_image_sku" {
+  type        = string
+  description = "Windows Image SKU."
+}
+
+variable "source_image_version" {
+  type        = string
+  description = "Windows Image Version."
+}
+
+source "azure-arm" "avd" {
+  # WinRM Communicator
+
+  communicator   = "winrm"
+  winrm_use_ssl  = true
+  winrm_insecure = true
+  winrm_timeout  = "5m"
+  winrm_username = "packer"
+
+  # Service Principal Authentication
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+
+  # Source Image
+
+  os_type         = "Windows"
+  image_publisher = var.source_image_publisher
+  image_offer     = var.source_image_offer
+  image_sku       = var.source_image_sku
+  image_version   = var.source_image_version
+
+  # Destination Image
+
+  managed_image_resource_group_name = var.artifacts_resource_group
+  managed_image_name                = "$[{var.source_image_sku}]-$[{var.source_image_version}]"
+
+  # Packer Computing Resources
+
+  build_resource_group_name = var.build_resource_group
+  vm_size                   = "Standard_D4ds_v4"
+}
+
+build {
+  source "azure-arm.avd" {}
+
+  # Install Chocolatey: https://chocolatey.org/install#individual
+  provisioner "powershell" {
+    inline = ["Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"]
+  }
+
+  # Install Chocolatey packages
+  provisioner "file" {
+    source      = "./packages.config"
+    destination = "D:/packages.config"
+  }
+
+  provisioner "powershell" {
+    inline = ["choco install --confirm D:/packages.config"]
+    # See https://docs.chocolatey.org/en-us/choco/commands/install#exit-codes
+    valid_exit_codes = [0, 3010]
+  }
+
+  provisioner "windows-restart" {}
+
+  # Azure PowerShell Modules
+  #provisioner "powershell" {
+  #  script = "./install-azure-powershell.ps1"
+  #}
+
+  # Generalize image using Sysprep
+  # See https://www.packer.io/docs/builders/azure/arm#windows
+  # See https://docs.microsoft.com/en-us/azure/virtual-machines/windows/build-image-with-packer#define-packer-template
+  provisioner "powershell" {
+    inline = [
+      "while ((Get-Service RdAgent).Status -ne 'Running') { Start-Sleep -s 5 }",
+      "while ((Get-Service WindowsAzureGuestAgent).Status -ne 'Running') { Start-Sleep -s 5 }",
+      "& $env:SystemRoot\\System32\\Sysprep\\Sysprep.exe /oobe /generalize /quiet /quit /mode:vm",
+      "while ($true) { $imageState = Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Setup\\State | Select ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }"
+    ]
+  }
+}
+#End
+EOT
+  
+  commit_message      = "Create windows.pkr.hcl"
+  overwrite_on_create = true
+}
+
+
 # Outputs to run Packer locally
 
 output "packer_artifacts_resource_group" {
