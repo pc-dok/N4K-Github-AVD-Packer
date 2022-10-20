7c34331f67d9effb99ab5427d4c3cdc0cc5f0260
pc-dok <info@n4k.at>
2022-10-20 18:03:35 +0200

    Update main.tf

--- 3_bastion/main.tf
+++ 3_bastion/main.tf
@@ -8,12 +8,12 @@ data "azurerm_image" "win2022" {
   
 # Import Data from the AADDS
   
-data "azurerm_virtual_network" "vnet-we-aadds.id" {
-  name                = "azurerm_virtual_network.vnet-we-aadds.id"
+data "azurerm_virtual_network" "vnet-we-aadds" {
+  id                  = "azurerm_virtual_network.vnet-we-aadds.id"
   resource_group_name = "n4k-we-aadds"
 }
   
-data "azurerm_virtual_network" "vnet-we-aadds.name" {
+data "azurerm_virtual_network" "vnet-we-aadds" {
   name                = "azurerm_virtual_network.vnet-we-aadds.name"
   resource_group_name = "n4k-we-aadds"
 }
