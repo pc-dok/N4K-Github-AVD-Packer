0e5b5501a8f250fc4815b8b487b9035d5268b82f
pc-dok <info@n4k.at>
2022-10-20 20:01:11 +0200

    Create providers.tf

---
+++ 4_avd/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "N4K"
+
+    workspaces {
+      name = "4_Github-AVD-WVD"
+    }
+  }
+  
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.10"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.ARM_SUBSCRIPTION
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  features {}
+}
+
+provider "github" {
+  token = var.GITHUB_SECRET
+  owner = var.GITHUB_OWNER
+}
