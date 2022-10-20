434a66bf00caf948437af411f194b7a5cfe8573e
pc-dok <info@n4k.at>
2022-10-20 19:22:52 +0200

    yo

--- 3_bastion/main.tf
+++ 3_bastion/main.tf
@@ -186,6 +186,29 @@ resource "azurerm_key_vault" "kv1" {
   }
 }
 
+resource "azurerm_key_vault_access_policy" "ad_group_policy" {
+  key_vault_id = azurerm_key_vault.kv1.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_user.info.object_id
+  
+  key_permissions = [
+      "Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey",
+    ]
+
+    secret_permissions = [
+      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
+    ]
+
+    certificate_permissions = [
+      "Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "Restore", "SetIssuers", "Update",
+    ]
+    
+    storage_permissions = [
+      "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update",
+    ]
+}
+
 # Create KeyVault VM passwords
 resource "random_password" "dcadmin" {
   length = 20
