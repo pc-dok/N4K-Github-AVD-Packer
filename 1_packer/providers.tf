7d9435bf58890afce9fd7eb61570e6317809b5a3
pc-dok <info@n4k.at>
2022-11-16 14:19:46 +0100

    Update providers.tf

--- 1_packer/providers.tf
+++ 1_packer/providers.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = var.tf-org
+    organization = "N4K"
 
     workspaces {
-      name = var.tf-ws-github
+      name = "1_Github-AVD-Packer"
     }
   }
   
